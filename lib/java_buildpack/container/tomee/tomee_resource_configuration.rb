4a168b42b0687c3a15b459a86751f7a253d1f7ac
Violeta Georgieva <milesg78@gmail.com>
2015-11-28 22:02:14 +0200

    Add auto configuration for service bound to the application. This functionality will expose them as standard resources in TomEE.

---
+++ lib/java_buildpack/container/tomee/tomee_resource_configuration.rb
@@ -0,0 +1,99 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'java_buildpack/component/versioned_dependency_component'
+require 'java_buildpack/container'
+require 'java_buildpack/container/tomcat/tomcat_utils'
+
+module JavaBuildpack
+  module Container
+
+    # Encapsulates the detect, compile, and release functionality for TomEE resources configuration.
+    class TomeeResourceConfiguration < JavaBuildpack::Component::VersionedDependencyComponent
+      include JavaBuildpack::Container
+
+      # (see JavaBuildpack::Component::BaseComponent#initialize)
+      def initialize(context)
+        super(context)
+        @logger = JavaBuildpack::Logging::LoggerFactory.instance.get_logger TomeeResourceConfiguration
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#compile)
+      def compile
+        download_jar(jar_name, tomcat_lib)
+        return unless @configuration['enabled']
+        mutate_resources_xml
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#release)
+      def release
+      end
+
+      protected
+
+      # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
+      def supports?
+        true
+      end
+
+      private
+
+      def jar_name
+        "tomee_resource_configuration-#{@version}.jar"
+      end
+
+      def mutate_resources_xml
+        with_timing 'Modifying /WEB-INF/resources.xml for Resource Configuration' do
+          document = read_xml resources_xml
+          @logger.debug { "  Original resources.xml: #{document}" }
+
+          resources  = REXML::XPath.match(document, '/resources').first
+          resources  = document.add_element 'resources' if resources.nil?
+
+          relational_services_as_resources resources
+
+          write_xml resources_xml, document
+          @logger.debug { "  Modified resources.xml: #{document}" }
+        end
+      end
+
+      def relational_services_as_resources(resources)
+        @application.services.each do |service|
+          next unless service['tags'].include? 'relational'
+          add_relational_resource service, resources
+        end
+      end
+
+      def add_relational_resource(service, resources)
+        resources.add_element 'Resource',
+                              'id' => "jdbc/#{service['name']}",
+                              'type' => 'DataSource',
+                              'properties-provider' =>
+                              'org.cloudfoundry.reconfiguration.tomee.DelegatingPropertiesProvider'
+      end
+
+      def resources_xml
+        @droplet.root + 'WEB-INF/resources.xml'
+      end
+
+      def read_xml(file)
+        File.open(file, 'a+') { |f| REXML::Document.new f }
+      end
+
+    end
+
+  end
+end
---
+++ spec/java_buildpack/container/tomee/tomee_resource_configuration_spec.rb
@@ -0,0 +1,121 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'component_helper'
+require 'java_buildpack/container/tomee/tomee_resource_configuration'
+
+describe JavaBuildpack::Container::TomeeResourceConfiguration do
+  include_context 'component_helper'
+
+  let(:component_id) { 'tomee' }
+
+  let(:configuration) { { 'enabled' => true } }
+
+  it 'always detects' do
+    expect(component.detect).to eq("tomee-resource-configuration=#{version}")
+  end
+
+  it 'copies resources',
+     cache_fixture: 'stub-resource-configuration.jar',
+     app_fixture: 'container_tomcat' do
+
+    component.compile
+
+    expect(sandbox + "lib/tomee_resource_configuration-#{version}.jar").to exist
+  end
+
+  it 'does not create Resource entries if there are no services',
+     cache_fixture: 'stub-resource-configuration.jar',
+     app_fixture: 'container_tomcat' do
+
+    component.compile
+
+    web_inf = app_dir + 'WEB-INF'
+    resources_xml = web_inf + 'resources.xml'
+    expect(resources_xml).to exist
+    expect(resources_xml).not_to match(/<Resource/)
+  end
+
+  context do
+    let(:configuration) { { 'enabled' => false } }
+
+    it 'does not create resources.xml if component is disabled',
+       cache_fixture: 'stub-resource-configuration.jar',
+       app_fixture: 'container_tomcat' do
+
+      component.compile
+
+      web_inf = app_dir + 'WEB-INF'
+      resources_xml = web_inf + 'resources.xml'
+      expect(resources_xml).not_to exist
+    end
+  end
+
+  context do
+
+    before do
+      allow(services).to receive(:each).and_yield('name' => 'test',
+                                                  'tags' => ['relational'])
+    end
+
+    it 'creates resources.xml if not present',
+       cache_fixture: 'stub-resource-configuration.jar',
+       app_fixture: 'container_tomcat' do
+
+      web_inf = app_dir + 'WEB-INF'
+      resources_xml = web_inf + 'resources.xml'
+      expect(resources_xml).not_to exist
+
+      component.compile
+
+      expect(resources_xml).to exist
+    end
+
+    it 'adds Resource element to empty resources.xml with root element only',
+       cache_fixture: 'stub-resource-configuration.jar',
+       app_fixture: 'container_tomee_empty_resources_xml' do
+
+      web_inf = app_dir + 'WEB-INF'
+      resources_xml = web_inf + 'resources.xml'
+      expect(resources_xml).to exist
+
+      component.compile
+
+      expect(resources_xml.read).to match(
+        %r{<Resource id='jdbc/test' type='DataSource'\
+ properties-provider='org.cloudfoundry.reconfiguration.tomee.DelegatingPropertiesProvider'/>})
+    end
+
+    it 'adds Resource element to non-empty resources.xml',
+       cache_fixture: 'stub-resource-configuration.jar',
+       app_fixture: 'container_tomee_nonempty_resources_xml' do
+
+      web_inf = app_dir + 'WEB-INF'
+      resources_xml = web_inf + 'resources.xml'
+      expect(resources_xml).to exist
+
+      component.compile
+
+      expect(resources_xml.read).to match(
+        %r{<Resource id='My Test Resource' type='my.test.Resource' provider='my.test#Provider'/>})
+      expect(resources_xml.read).to match(
+        %r{<Resource id='jdbc/test' type='DataSource'\
+ properties-provider='org.cloudfoundry.reconfiguration.tomee.DelegatingPropertiesProvider'/>})
+    end
+  end
+
+end
