457242cc05c4386501be2cfca2f3d8cfe506c77f
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-04-28 22:11:52 -0700

    GH-16: Support for deploying ear files via tomee buildpack
    Signed-off-by: Anwar Chirakkattil <achirakkattil@pivotal.io>

--- lib/java_buildpack/container/tomee.rb
+++ lib/java_buildpack/container/tomee.rb
@@ -68,10 +68,14 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::ModularComponent#supports?)
       def supports?
-        web_inf? && !JavaBuildpack::Util::JavaMainUtils.main_class(@application)
+        (web_inf? && !JavaBuildpack::Util::JavaMainUtils.main_class(@application)) || is_ear?
       end
 
       private
+
+      def is_ear?
+        (@application.root + 'META-INF/application.xml').exist?
+      end
 
       def web_inf?
         (@application.root + 'WEB-INF').exist?
